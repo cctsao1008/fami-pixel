edc049e83a96896319927f1b6baaaa14b137796e
Cheng <chiacheng.tsao@gmail.com>
2026-09-17 02:22:34 +0800

    integration: expose external substrate port

---
+++ src/fami_pixel/integration/__init__.py
@@ -0,0 +1,19 @@
+from .external_substrate import (
+    BoundedAction,
+    CommandChunk,
+    FamiObservation,
+    LiveConsequence,
+    LiveExecutionTrace,
+    ShadowRollout,
+    Smb1ExternalSubstratePort,
+)
+
+__all__ = [
+    "BoundedAction",
+    "CommandChunk",
+    "FamiObservation",
+    "LiveConsequence",
+    "LiveExecutionTrace",
+    "ShadowRollout",
+    "Smb1ExternalSubstratePort",
+]
