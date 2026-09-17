ceb293e51640457c1b9eab60dbdd2a3f02910628
Cheng <chiacheng.tsao@gmail.com>
2026-09-17 13:09:37 +0800

    ml: export surrogate artifact contract helpers

--- src/fami_pixel/learning/__init__.py
+++ src/fami_pixel/learning/__init__.py
@@ -1,5 +1,19 @@
 """Learning-side utilities for predictive models built from Mesen rollout data."""
 
+from .model_artifact import (
+    ARTIFACT_FORMAT,
+    AUTHORITY_BOUNDARY,
+    DATASET_MANIFEST_FORMAT,
+    METRICS_FORMAT,
+    TARGET_SCHEMA_ID,
+    build_dataset_manifest,
+    build_metadata,
+    build_metrics,
+    load_model_artifact,
+    next_versioned_artifact_dir,
+    sha256_file,
+    write_model_artifact,
+)
 from .rollout_dataset import (
     ROLLOUT_SCHEMA_VERSION,
     build_rollout_record,
@@ -7,11 +21,35 @@ from .rollout_dataset import (
     summarize_rollout_records,
     write_jsonl_record,
 )
+from .tiny_mlp import (
+    FEATURE_SCHEMA_ID,
+    FEATURE_VECTOR_SIZE,
+    MODEL_FORMAT,
+    OUTPUT_SCHEMA,
+    TinySurrogateMLP,
+)
 
 __all__ = [
+    "ARTIFACT_FORMAT",
+    "AUTHORITY_BOUNDARY",
+    "DATASET_MANIFEST_FORMAT",
+    "FEATURE_SCHEMA_ID",
+    "FEATURE_VECTOR_SIZE",
+    "METRICS_FORMAT",
+    "MODEL_FORMAT",
+    "OUTPUT_SCHEMA",
     "ROLLOUT_SCHEMA_VERSION",
+    "TARGET_SCHEMA_ID",
+    "TinySurrogateMLP",
+    "build_dataset_manifest",
+    "build_metadata",
+    "build_metrics",
     "build_rollout_record",
     "load_jsonl_records",
+    "load_model_artifact",
+    "next_versioned_artifact_dir",
+    "sha256_file",
     "summarize_rollout_records",
     "write_jsonl_record",
+    "write_model_artifact",
 ]
