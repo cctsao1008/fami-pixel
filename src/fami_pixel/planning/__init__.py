c349e57393d21a2d566b93f52384410d09027363
Cheng <chiacheng.tsao@gmail.com>
2026-09-18 22:13:43 +0800

    planning: export COLLECT proof selection seam

--- src/fami_pixel/planning/__init__.py
+++ src/fami_pixel/planning/__init__.py
@@ -8,14 +8,24 @@ from .collect_handoff import (
     evaluate_handoff_stage,
     ordered_handoffs,
 )
+from .collect_selection import (
+    CollectProofSelector,
+    CollectSelectionLike,
+    select_collect_proof,
+    shape_eager_collect_result,
+)
 from .contracts import PlanSelector
 
 __all__ = [
     "CollectHandoffStage",
+    "CollectProofSelector",
+    "CollectSelectionLike",
     "PlanSelector",
     "active_worker_ids",
     "collect_anchor_proofs",
     "collect_handoff_proofs",
     "evaluate_handoff_stage",
     "ordered_handoffs",
+    "select_collect_proof",
+    "shape_eager_collect_result",
 ]
