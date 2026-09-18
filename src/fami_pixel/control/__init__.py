98122452facccc05f2950223bff88c7333fccfda
Cheng <chiacheng.tsao@gmail.com>
2026-09-18 21:18:29 +0800

    control: export checkpoint request enrichment

--- src/fami_pixel/control/__init__.py
+++ src/fami_pixel/control/__init__.py
@@ -2,5 +2,18 @@
 
 from .authority_plan import AuthorityPlanMemory
 from .delegates import PlanDelegateSlot
+from .request_enrichment import (
+    AuthorityContinuationRequestEnricher,
+    RequestPayloadEnricherSlot,
+    enrich_checkpoint_request,
+    installed_checkpoint_request_enricher,
+)
 
-__all__ = ["AuthorityPlanMemory", "PlanDelegateSlot"]
+__all__ = [
+    "AuthorityContinuationRequestEnricher",
+    "AuthorityPlanMemory",
+    "PlanDelegateSlot",
+    "RequestPayloadEnricherSlot",
+    "enrich_checkpoint_request",
+    "installed_checkpoint_request_enricher",
+]
