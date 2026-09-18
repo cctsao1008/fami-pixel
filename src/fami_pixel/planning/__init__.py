86d63f361af813aea1d5dc1a6838b3e3003813b8
Cheng <chiacheng.tsao@gmail.com>
2026-09-18 21:33:06 +0800

    planning: export COLLECT handoff admission primitives

--- src/fami_pixel/planning/__init__.py
+++ src/fami_pixel/planning/__init__.py
@@ -1,5 +1,21 @@
 """Stable planning contracts and pure policy primitives."""
 
+from .collect_handoff import (
+    CollectHandoffStage,
+    active_worker_ids,
+    collect_anchor_proofs,
+    collect_handoff_proofs,
+    evaluate_handoff_stage,
+    ordered_handoffs,
+)
 from .contracts import PlanSelector
 
-__all__ = ["PlanSelector"]
+__all__ = [
+    "CollectHandoffStage",
+    "PlanSelector",
+    "active_worker_ids",
+    "collect_anchor_proofs",
+    "collect_handoff_proofs",
+    "evaluate_handoff_stage",
+    "ordered_handoffs",
+]
