2efac2f2319e5f3482b6243e2974bb7380cf1825
Cheng <chiacheng.tsao@gmail.com>
2026-09-18 21:10:48 +0800

    control: export authority plan memory

--- src/fami_pixel/control/__init__.py
+++ src/fami_pixel/control/__init__.py
@@ -1,5 +1,6 @@
 """Stable live-control composition helpers."""
 
+from .authority_plan import AuthorityPlanMemory
 from .delegates import PlanDelegateSlot
 
-__all__ = ["PlanDelegateSlot"]
+__all__ = ["AuthorityPlanMemory", "PlanDelegateSlot"]
